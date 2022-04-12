da0ad6e4fbd8f97a14b9a55650fb9ea5254a23d5
Adriaan de Vos <adriaan.devos@gmail.com>
2022-04-12 12:11:20 +0200

    Add debug sequence number to send source functionality

--- scapy/send-source.py
+++ scapy/send-source.py
@@ -3,10 +3,12 @@ from scapy.layers.l2 import Ether
 from scapy.sendrecv import sendp
 import time
 
-a = Ether(src="6c:5a:b0:b3:a2:c6", dst="61:64:72:69:61:6e") / IP(src="10.0.0.1", dst="10.0.0.2") / "Keep On Driving..."
+a = Ether(src="6c:5a:b0:b3:a2:c6", dst="61:64:72:69:61:6e") / IP(src="10.0.0.1", dst="10.0.0.2")
 
+counter = 0
 while True:
-    sendp(a, iface="eth2")
+    counter += 1
+    sendp(a / (str(counter) + " Keep On Driving..."), iface="eth2")
     time.sleep(1)
 
 
