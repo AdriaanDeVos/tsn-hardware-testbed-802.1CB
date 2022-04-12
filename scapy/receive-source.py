cb7ff7f16721a0ff55dc182a45ad6b400be3a7d2
Adriaan de Vos <adriaan.devos@gmail.com>
2022-04-12 12:11:08 +0200

    Improve receive output format

--- scapy/receive-source.py
+++ scapy/receive-source.py
@@ -7,11 +7,18 @@
 import time
 
 from RedundancyTag import RedundancyTag
-from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP
+from scapy.packet import Raw
 from scapy.sendrecv import AsyncSniffer
 
+
+def print_raw(pkt):
+    if pkt.haslayer(Raw) and pkt[IP].dst == "10.0.0.2":
+        print(pkt[Raw].load.decode("utf-8").rstrip('\x00'))
+
+
 """Sniff all incoming packets"""
-sniffer = AsyncSniffer(iface="eth1", prn=Ether.show)
+sniffer = AsyncSniffer(iface="eth1", prn=print_raw)
 sniffer.start()
 
 while True:
